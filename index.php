f4eed24e6c097f5ae240c93bb15664d3e93e1c61
JayRawlani <46822412+JayRawlani@users.noreply.github.com>
2020-10-22 01:04:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -38,10 +38,6 @@
             #demo1{
                 margin-top: 50px;
             }
-            .carousel-inner img {
-                width: 100%;
-                height: 100%;
-            }
             #home{
                 width: 100%;
                 margin: 0px auto;
@@ -114,39 +110,6 @@
             </p>
         </h4>            
 
-        </div>
-            <div id="demo" class="carousel slide" data-ride="carousel">
-
-                <!-- Indicators -->
-                <ul class="carousel-indicators">
-                  <li data-target="#demo" data-slide-to="0" class="active"></li>
-                  <li data-target="#demo" data-slide-to="1"></li>
-                  <li data-target="#demo" data-slide-to="2"></li>
-                </ul>
-                
-                <!-- The slideshow -->
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="1.jpg" alt="Los Angeles" width="50%" height="50%">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="2.jpg" alt="Chicago" width="50%" height="50%">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="3.jpg" alt="New York" width="50%" height="50%">
-                  </div>
-                </div>
-                
-                <!-- Left and right controls -->
-                <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                  <span class="carousel-control-prev-icon"></span>
-                </a>
-                <a class="carousel-control-next" href="#demo" data-slide="next">
-                  <span class="carousel-control-next-icon"></span>
-                </a>
-              </div>
-            
-            </div>
         <div id="register_form">
             
             <div class="container" style="margin-left: 30%;">
